d55f933c6d6ec3815e0c081e3101b6f39e09ccc7
Yam0 <tage.akerstrom@gmail.com>
2021-06-18 23:42:55 +0200

    Standard setup

---
+++ Assets/Scripts/DroneController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneController : MonoBehaviour
+{
+	public float speed;
+	private Vector2 direction;
+
+	void Start()
+	{
+
+	}
+
+	void Update()
+	{
+
+	}
+}
